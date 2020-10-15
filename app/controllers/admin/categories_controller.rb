4b083eb5de2369fa182ab32b0d66392ac260461e
David Ramalho <david.m.ramalho@gmail.com>
2020-10-15 18:26:05 +0000

    selecting for products in categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,8 +2,16 @@ class Admin::CategoriesController < ApplicationController
 
   def index
     @categories = Category.order(id: :desc).all
+    @categoryId = []
+    @categories.each do |id|
+      @categoryId.push(id[:id].to_i)
+    end
+    @categoryId.each do |id|
+      @productsWithCategory = Product.where("category_id = #{id}")
+      @productCount = @productsWithCategory.count
+    end
+    
     @count = Category.count
-    @productCount = Product.count
   end
 
   def new
@@ -24,6 +32,7 @@ class Admin::CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(
+      :id, 
       :name
     )
   end
